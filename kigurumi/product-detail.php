5b5e644079b66d52f5d4af9ef0ba0fcfee387cce
Emadon13 <38349700+Emadon13@users.noreply.github.com>
2018-10-11 13:12:08 +0200

    product details 1

--- kigurumi/product-detail.php
+++ kigurumi/product-detail.php
@@ -33,8 +33,13 @@
 </head>
 <body class="animsition">
 
+
+
+
 	<!-- Header -->
 	<?php include("entete.php"); ?>
+
+
 
 	<!-- breadcrumb -->
 	<div class="bread-crumb bgwhite flex-w p-l-52 p-r-15 p-t-30 p-l-15-sm">
@@ -58,153 +63,177 @@
 		</span>
 	</div>
 
-	<!-- Product Detail -->
-	<div class="container bgwhite p-t-35 p-b-80">
-		<div class="flex-w flex-sb">
-			<div class="w-size13 p-t-30 respon5">
-				<div class="wrap-slick3 flex-sb flex-w">
-					<div class="wrap-slick3-dots"></div>
 
-					<div class="slick3">
-						<div class="item-slick3" data-thumb="images/thumb-item-01.jpg">
-							<div class="wrap-pic-w">
-								<img src="images/product-detail-01.jpg" alt="IMG-PRODUCT">
+	<!-- Ajout de la base de donnée -->
+
+	<?php
+		try
+		{
+    	$bdd = new PDO('mysql:host=localhost;dbname=kigurumi;charset=utf8', 'root', '');
+		}
+
+		catch (Exception $e)
+		{
+    	die('Erreur : ' . $e->getMessage());
+		}
+
+		$reponse = $bdd->query('SELECT * FROM products WHERE ID = 1');
+		while ($donnees = $reponse->fetch())
+		{
+			?>
+
+				<!-- Product Detail -->
+				<div class="container bgwhite p-t-35 p-b-80">
+					<div class="flex-w flex-sb">
+						<div class="w-size13 p-t-30 respon5">
+							<div class="wrap-slick3 flex-sb flex-w">
+								<div class="wrap-slick3-dots"></div>
+
+								<div class="slick3">
+									<div class="item-slick3" data-thumb="images/thumb-item-01.jpg">
+										<div class="wrap-pic-w">
+											<img src="images/<?php echo $donnees['ImageName']; ?>" alt="IMG-PRODUCT">
+										</div>
+									</div>
+
+									<div class="item-slick3" data-thumb="images/thumb-item-02.jpg">
+										<div class="wrap-pic-w">
+											<img src="images/product-detail-02.jpg" alt="IMG-PRODUCT">
+										</div>
+									</div>
+
+									<div class="item-slick3" data-thumb="images/thumb-item-03.jpg">
+										<div class="wrap-pic-w">
+											<img src="images/product-detail-03.jpg" alt="IMG-PRODUCT">
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 
-						<div class="item-slick3" data-thumb="images/thumb-item-02.jpg">
-							<div class="wrap-pic-w">
-								<img src="images/product-detail-02.jpg" alt="IMG-PRODUCT">
-							</div>
-						</div>
+						<div class="w-size14 p-t-30 respon5">
+							<h4 class="product-detail-name m-text16 p-b-13">
+								<?php echo $donnees['Nom']; ?>
+							</h4>
 
-						<div class="item-slick3" data-thumb="images/thumb-item-03.jpg">
-							<div class="wrap-pic-w">
-								<img src="images/product-detail-03.jpg" alt="IMG-PRODUCT">
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+							<span class="m-text17">
+								<?php echo $donnees['Prix']; ?>€
+							</span>
 
-			<div class="w-size14 p-t-30 respon5">
-				<h4 class="product-detail-name m-text16 p-b-13">
-					Boxy T-Shirt with Roll Sleeve Detail
-				</h4>
+							<p class="s-text8 p-t-10">
+								Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
+							</p>
 
-				<span class="m-text17">
-					$22
-				</span>
+							<!--  -->
+							<div class="p-t-33 p-b-60">
+								<div class="flex-m flex-w p-b-10">
+									<div class="s-text15 w-size15 t-center">
+										Taille
+									</div>
 
-				<p class="s-text8 p-t-10">
-					Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
-				</p>
+									<div class="rs2-select2 rs3-select2 bo4 of-hidden w-size16">
+										<select class="selection-2" name="size">
+											<option>Choisissez votre taille !</option>
+											<option> M (taille unique)</option>
+										</select>
+									</div>
+								</div>
 
-				<!--  -->
-				<div class="p-t-33 p-b-60">
-					<div class="flex-m flex-w p-b-10">
-						<div class="s-text15 w-size15 t-center">
-							Size
-						</div>
+								<!-- <div class="flex-m flex-w">
+									<div class="s-text15 w-size15 t-center">
+										Color
+									</div>
 
-						<div class="rs2-select2 rs3-select2 bo4 of-hidden w-size16">
-							<select class="selection-2" name="size">
-								<option>Choose an option</option>
-								<option>Taille M (taille unique)</option>
-							</select>
-						</div>
-					</div>
+									<div class="rs2-select2 rs3-select2 bo4 of-hidden w-size16">
+										<select class="selection-2" name="color">
+											<option>Choose an option</option>
+											<option>Gray</option>
+											<option>Red</option>
+											<option>Black</option>
+											<option>Blue</option>
+										</select>
+									</div>
+								</div> -->
 
-					<!-- <div class="flex-m flex-w">
-						<div class="s-text15 w-size15 t-center">
-							Color
-						</div>
+								<div class="flex-r-m flex-w p-t-10">
+									<div class="w-size16 flex-m flex-w">
+										<div class="flex-w bo5 of-hidden m-r-22 m-t-10 m-b-10">
+											<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
+												<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
+											</button>
 
-						<div class="rs2-select2 rs3-select2 bo4 of-hidden w-size16">
-							<select class="selection-2" name="color">
-								<option>Choose an option</option>
-								<option>Gray</option>
-								<option>Red</option>
-								<option>Black</option>
-								<option>Blue</option>
-							</select>
-						</div>
-					</div> -->
+											<input class="size8 m-text18 t-center num-product" type="number" name="num-product" value="1">
 
-					<div class="flex-r-m flex-w p-t-10">
-						<div class="w-size16 flex-m flex-w">
-							<div class="flex-w bo5 of-hidden m-r-22 m-t-10 m-b-10">
-								<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-									<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-								</button>
+											<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
+												<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
+											</button>
+										</div>
 
-								<input class="size8 m-text18 t-center num-product" type="number" name="num-product" value="1">
-
-								<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-									<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-								</button>
+										<div class="btn-addcart-product-detail size9 trans-0-4 m-t-10 m-b-10">
+											<!-- Button -->
+											<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
+												Ajouter au panier
+											</button>
+										</div>
+									</div>
+								</div>
 							</div>
 
-							<div class="btn-addcart-product-detail size9 trans-0-4 m-t-10 m-b-10">
-								<!-- Button -->
-								<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-									Add to Cart
-								</button>
+							<div class="p-b-45">
+								<span class="s-text8 m-r-35">ID : <?php echo $donnees['ID']; ?></span>
+								<span class="s-text8">Catégorie : <?php echo $donnees['Type']; ?></span>
+							</div>
+
+							<!--  -->
+							<div class="wrap-dropdown-content bo6 p-t-15 p-b-14 active-dropdown-content">
+								<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
+									Description
+									<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
+									<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
+								</h5>
+
+								<div class="dropdown-content dis-none p-t-15 p-b-23">
+									<p class="s-text8">
+										<?php echo $donnees['Description']; ?>
+									</p>
+								</div>
+							</div>
+
+							<div class="wrap-dropdown-content bo7 p-t-15 p-b-14">
+								<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
+									Informations supplémentaires
+									<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
+									<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
+								</h5>
+
+								<div class="dropdown-content dis-none p-t-15 p-b-23">
+									<p class="s-text8">
+										Le saviez-vous ? Nous n'avons pas encore ce champs dans notre bdd
+								</div>
+							</div>
+
+							<div class="wrap-dropdown-content bo7 p-t-15 p-b-14">
+								<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
+									Commentaires (0)
+									<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
+									<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
+								</h5>
+
+								<div class="dropdown-content dis-none p-t-15 p-b-23">
+									<p class="s-text8">
+										Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 
-				<div class="p-b-45">
-					<span class="s-text8 m-r-35">SKU: MUG-01</span>
-					<span class="s-text8">Categories: Mug, Design</span>
-				</div>
+			<?php
+		}
+		$reponse->closeCursor();
+	?>
 
-				<!--  -->
-				<div class="wrap-dropdown-content bo6 p-t-15 p-b-14 active-dropdown-content">
-					<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
-						Description
-						<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
-						<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
-					</h5>
-
-					<div class="dropdown-content dis-none p-t-15 p-b-23">
-						<p class="s-text8">
-							Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
-						</p>
-					</div>
-				</div>
-
-				<div class="wrap-dropdown-content bo7 p-t-15 p-b-14">
-					<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
-						Additional information
-						<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
-						<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
-					</h5>
-
-					<div class="dropdown-content dis-none p-t-15 p-b-23">
-						<p class="s-text8">
-							Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
-						</p>
-					</div>
-				</div>
-
-				<div class="wrap-dropdown-content bo7 p-t-15 p-b-14">
-					<h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4">
-						Reviews (0)
-						<i class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
-						<i class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
-					</h5>
-
-					<div class="dropdown-content dis-none p-t-15 p-b-23">
-						<p class="s-text8">
-							Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 
 
 	<!-- Relate Product -->
