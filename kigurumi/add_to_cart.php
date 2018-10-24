d12aa71e6a2df07011bd82e1c5d762fe2b9ec4b6
killingbeta <robi61@sfr.fr>
2018-10-24 15:18:04 +0200

    add_to_cart terminé

--- kigurumi/add_to_cart.php
+++ kigurumi/add_to_cart.php
@@ -12,13 +12,22 @@ if (isset($_GET['add'])) {
   $donnees = $req->fetch();
   $req->closeCursor();
   if ($donnees['COUNT(Nom)'] == 1) {
-    $_SESSION['cart'][] = array('nom' => $item,
-                                'quantite' => 1);
+    $found = false;
+    for ($i=0; $i < count($_SESSION['cart']) && !$found; $i++) {
+      if($_SESSION['cart'][i]['nom'] == $item) {
+        $found = true;
+        $_SESSION['cart'][i]['quantite']++;
+      }
+    }
+    if(!$found) {
+      $_SESSION['cart'][] = array('nom' => $item,
+                                  'quantite' => 1);
+    }
     $req = $bdd->prepare('SELECT Prix FROM products WHERE Nom=?');
     $req->execute(array($item));
     $prix = $req->fetch();
     $req->closeCursor();
-    $_SESSION['cart_total'] = $_SESSION['cart_total'] + $prix['Prix'];
+    $_SESSION['cart_total'] = round($_SESSION['cart_total'] + $prix['Prix'], 2);
   }
 }
 elseif (isset($_GET['remove'])) {
@@ -33,7 +42,28 @@ elseif (isset($_GET['remove'])) {
       $req->execute(array($item));
       $prix = $req->fetch();
       $req->closeCursor();
-      $_SESSION['cart_total'] = $_SESSION['cart_total'] - $prix['Prix'];
+      $_SESSION['cart_total'] = round($_SESSION['cart_total'] - $prix['Prix'], 2);
+    }
+  }
+}
+elseif (isset($_GET['edit']) && isset($_GET['q'])) {
+  $item = htmlspecialchars($_GET['edit'], ENT_QUOTES, 'UTF-8');
+  $quantity = htmlspecialchars($_GET['q'], ENT_QUOTES, 'UTF-8');
+  if(is_numeric($quantity)) {
+    for ($i=0; $i < count($_SESSION['cart']); $i++) {
+      if ($_SESSION['cart'][$i]['nom'] == $item)
+      {
+        $oldquantity = $_SESSION['cart'][$i]['quantity'];
+        if($oldquantity != floatval($quantity)) {
+          $_SESSION['cart'][$i]['quantity'] = floatval($quantity);
+          $bdd = new PDO('mysql:host=localhost;dbname=kigurumi;charset=utf8', 'root', '');
+          $req = $bdd->prepare('SELECT Prix FROM products WHERE Nom=?');
+          $req->execute(array($item));
+          $prix = $req->fetch();
+          $req->closeCursor();
+          $_SESSION['cart_total'] = round($_SESSION['cart_total'] + $oldquantity * $prix['Prix'], 2);
+        }
+      }
     }
   }
 }
