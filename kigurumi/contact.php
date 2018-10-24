b0b094385a79c1e52c30f8c8a4695ff3a4d9788d
Bliisters <quentin.flegeau@orange.fr>
2018-10-24 18:25:18 +0200

    en cours

--- kigurumi/contact.php
+++ kigurumi/contact.php
@@ -110,8 +110,11 @@
 							<form class="leave-comment" action="contact.php" method="post">
 								<h4 class="m-text26 p-b-36 p-t-15">
 									Envoyez nous votre message
-								</h4>
+								</h4>';
 
+								if(!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
+
+								echo'
 								<div class="bo4 of-hidden size15 m-b-20">
 									<input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="name" placeholder="Nom complet*">
 								</div>
@@ -122,7 +125,11 @@
 
 								<div class="bo4 of-hidden size15 m-b-20">
 									<input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="email" placeholder="Adresse mail*">
-								</div>
+								</div>';
+
+							}
+
+							echo'
 
 								<textarea class="dis-block s-text7 size20 bo4 p-l-22 p-r-22 p-t-13 m-b-20" name="message" placeholder="Message"></textarea>
 
