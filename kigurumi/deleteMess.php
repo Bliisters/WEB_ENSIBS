e81450b23c438d74b92549eeb7db164768a1a1a0
killingbeta <robi61@sfr.fr>
2018-12-13 12:04:32 +0100

    logs ajoutés dans deleteMess

--- kigurumi/deleteMess.php
+++ kigurumi/deleteMess.php
@@ -8,23 +8,24 @@ if(!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
   header('location: index.php');
   exit;
 }
-
-else{
+else {
   $bdd = new PDO('mysql:host=localhost;dbname=kigurumi;charset=utf8', 'root', '');
   $req = $bdd->prepare('SELECT ID FROM employees WHERE ID_User = :id AND Type= "Admin"');
   $req->execute(array(
       'id' => $_SESSION['ID']));
   $count = $req->rowCount();
   if(!($count>0)){
+    $date = getdate();
+    $log = "[" + $date['mday'] + "/" + $date['mon'] + "/" + $date['year'] + " " + $date['hours'] + ":" + $date['minutes'] + ":" + $date['seconds'] + "] "
+    + "deleteMess.php session ID not employee: ID=" + $_SESSION['ID'] + "\n";
+    file_put_contents('logs/access.log', $log, FILE_APPEND);
     header('location: index.php');
     exit;
   }
   $req->closeCursor();
-  
-}
 
+}
 if(isset($_POST['ID_message']) && is_numeric($_POST['ID_message']) && isset($_POST['table']) && preg_match('/^[a-zA-Z_]+$/', $_POST['table'])) {
-  $bdd = new PDO('mysql:host=localhost;dbname=kigurumi;charset=utf8', 'root', '');
   $bdd->query('DELETE FROM ' . $_POST['table'] . ' WHERE ID_message = ' . $_POST['ID_message']);
 }
 else {
