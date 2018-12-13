377b80a4be03e9891fbdfb5adba3c041a8662fd1
Emadon13 <38349700+Emadon13@users.noreply.github.com>
2018-12-13 10:22:33 +0100

    product

--- kigurumi/product.php
+++ kigurumi/product.php
@@ -168,9 +168,9 @@
 							$reponse = $bdd->prepare('SELECT * FROM products WHERE Type = :type');
 							$reponse->execute(array(':type' => $_GET['type']));
 						}
-						elseif(isset($_GET['search-product']) && preg_match('/^([a-zA-Z0-9\-]+\040?)+$/', $_GET['search-product'])
+						elseif(isset($_GET['search-product']) && preg_match('/^([a-zA-Z0-9\-]+\040?)+$/', $_GET['search-product']))
 						{
-							elseif(isset($_GET['sorting'])){
+							if(isset($_GET['sorting'])){
 								if($_GET['sorting']=='popularity'){
 									$reponse = $bdd->prepare('SELECT * FROM products WHERE Nom LIKE :name');
 									$reponse->execute(array(':name' => '%'.$_GET['search-product'].'%'));
