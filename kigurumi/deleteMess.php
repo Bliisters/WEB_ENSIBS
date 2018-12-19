ef95d440bc27458404a7c09f2a8ac9196a7b5c8a
unknown <thibbbaut2975097@gmail.com>
2018-12-19 13:59:06 +0100

    Update deleteMess.php

--- kigurumi/deleteMess.php
+++ kigurumi/deleteMess.php
@@ -25,7 +25,8 @@ else {
   $req->closeCursor();
 
 }
-if(isset($_POST['ID_message']) && is_numeric($_POST['ID_message']) && isset($_POST['table']) && preg_match('/^[a-zA-Z_]+$/', $_POST['table'])) {
+if(isset($_POST['ID_message']) && is_numeric($_POST['ID_message']) && isset($_POST['table']) && preg_match('/^[a-zA-Z]+$/', $_POST['table'])) {
+  $bdd = new PDO('mysql:host=localhost;dbname=kigurumi;charset=utf8', 'root', '');
   $bdd->query('DELETE FROM ' . $_POST['table'] . ' WHERE ID_message = ' . $_POST['ID_message']);
 }
 else {
